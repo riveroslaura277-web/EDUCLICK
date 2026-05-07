d2fbb0eef0fa07f000a594f656460ed9f7a3f5cf
danamanrrique <danamanrrique23@gmail.com>
2026-05-07 14:30:49 -0500

    agregue toda la vista de roles

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using EDUCLICK.Models;
 namespace EDUCLICK.Controllers;
 
 public class HomeController : Controller
+
 {
     private readonly ILogger<HomeController> _logger;
 
@@ -18,9 +19,27 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult roles ()
+    public IActionResult Roles ()
     {
-        return View("~/Views/Roles/Roles.cshtml");
+        return View("~/Views/roless/fondoRoles.cshtml");
+    }
+    public IActionResult Docente()
+    {
+        return View("~/Views/vistaRoles/Docente.cshtml");
+    }
+    public IActionResult Acudiente ()
+    {
+        return View("~/Views/vistaRoles/Acudiente.cshtml");
+    }
+
+    public IActionResult Estudiante()
+    {
+        return View("~/Views/vistaRoles/Estudiante.cshtml");
+    }
+
+    public IActionResult Rector ()
+    {
+        return View("~/Views/vistaRoles/Rctor.cshtml");
     }
     public IActionResult Privacy()
     {
